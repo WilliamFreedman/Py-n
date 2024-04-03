b717a370300116ddd37547808c9a8bb78187aad2
mwabunoha <57103620+mwabunoha@users.noreply.github.com>
2024-04-03 13:13:54 -0400

    test cases in test.py

--- test/test.py
+++ test/test.py
@@ -1,2 +1,52 @@
-1
-print(1)
+# Declaration w/ assignment
+x : int = 5
+
+# Arithmetic w/ assignment
+result : float = 3.14 * (2 + 3)
+
+# Logical operators
+flag : bool = true && false || !true
+print(flag)
+
+# While loop w/ counter
+counter: int = 0
+while counter < 10:
+	counter += 1
+
+print(counter)
+
+
+# If-else conditional
+
+if x > 5:
+	y : int = 10
+else :
+	y : int = 20
+  
+print(y)
+
+
+# Nested control w/ break
+if x <= 100:
+	while x != 0 :
+		x -= 1
+		if x == 10 :
+			break
+print(x)
+
+# Type casting w/ control stmt
+x : int = 1
+y : float = x + 3.14
+
+if y > 4:
+	while x < 5 :
+		x += 1
+else:
+	print(x)
+
+# Class definition
+  class Circle:
+	  radius : int
+
+	  def area() -> float:
+		  return 3.14159 * (this.radius * this.radius)
